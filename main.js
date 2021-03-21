9e3e05d5a7bf178378724b2c94ec8c0190966c49
Calculon9 <calcs1212@gmail.com>
2021-03-21 18:49:38 +0800

    First commit
    Upload HTML, CSS, JS

---
+++ main.js
@@ -0,0 +1,148 @@
+
+$(document).ready(function(){
+    // Initialize 
+    letterBoxCreator()
+})
+
+// Callbacks
+
+function gameFinished(){
+    let word = $('.letter-box .word-letter').text();
+    //word = Array.from(document.querySelectorAll('.word-letter')).reduce((word,letter) => word+=letter.textContent,'');
+
+    // Get current number of guesses
+    let guesses = parseInt($('#guess-num').text());
+
+    // Get current streak
+    let streak = parseInt($('.score #score-num').text());
+
+    // If game won
+    if(word === currentWord.toUpperCase()) {
+
+        // Increase streak
+        streak+=1;
+        $('.score #score-num').text(streak);
+      
+        // Display current score
+        $('body .info').after(`<div class='well-done border'>
+                                 <p>WELL DONE!</p>
+                                 <p>Current score:&#160<span id="current-score"></span></p>
+                                 <button id="next-word">NEXT WORD</button>
+                               </div>`);  
+        $('.well-done #current-score').text(streak);
+        $('.well-done').fadeOut(0).fadeIn(300);
+       
+
+        // Add click event on 'Next word' button
+
+        $('.well-done #next-word').click(function(){
+            
+            // Remove current word
+            $('.word .letter-box').remove();
+
+            // Reset guesses
+            $('#guess-num').text(5)
+
+            // Reset keyboard letter colors
+            $('.letter').removeClass('letter-correct');
+            $('.letter').removeClass('letter-wrong');
+
+            // Get new word
+            letterBoxCreator();
+
+            // Reactivate keyboard click event
+            $('.keyboard .row .letter').on('click',letterClick);
+
+            // Remove 'Well done' message
+            $('.well-done').remove();
+        })
+
+        // Stop keyboard event listeners
+        $('.keyboard .row .letter').off('click',letterClick);
+    }
+
+    if(guesses <= 0) {
+        // Display final score
+        $('body .info').after(`<div class='game-over border'>
+                                 <p>GAME OVER</p>
+                                 <p>Your score:&#160<span id="game-over-score"></span></p>
+                                 <button id="play-again">PLAY AGAIN</button>
+                               </div>`);
+        $('.game-over #game-over-score').text(streak);
+        $('.game-over').fadeOut(0).fadeIn(300);
+        
+        // Add click event on 'Play again' button
+        $('.game-over #play-again').click(function(){
+            window.location.reload();
+        })
+
+        // Stop keyboard event listeners
+        $('.keyboard .row .letter').off('click',letterClick);
+    }
+}
+
+// Set up current word with empty letter-boxes
+
+function randomGenerator(){
+    return Math.round((Math.random() * (words.length - 1)));
+}
+
+let words = ['computer','chess','television','kitchen'];
+
+let randomIndex;
+
+let currentWord;
+
+function letterBoxCreator () {
+    randomIndex = randomGenerator();
+    currentWord = words[randomIndex];
+    currentWord.split('').forEach(function(letter) {
+        $('.word').append(`<div id="${letter}" class="letter-box center"><p class="word-letter"></p></div>`);
+    })
+    $('.word').fadeOut(0).fadeIn(400);
+}
+
+
+// Add click events to letters on keyboard
+
+$('.keyboard .row .letter').click(letterClick);
+
+function letterClick(e) {
+    
+    // Check if 'currentWord' contains clicked letter 
+    let check = [];
+
+    // Get current number of guesses
+    let guesses = parseInt($('#guess-num').text());
+
+    currentWord.split('').forEach(function(letter, i) {
+        
+        let clickedLetter = e.currentTarget.firstChild.textContent.toLocaleUpperCase();
+
+        if(clickedLetter === letter.toLocaleUpperCase()) {
+
+            // If current letter isn't only occurrence, skip current letter since 'Make letter appear in letter box' places all common letters in their word boxes in one iteration
+            if(i !== currentWord.lastIndexOf(currentWord[i])) {return;} 
+
+            // Change background of correctly guessed letter
+            $(e.currentTarget).addClass('letter-correct');
+
+            // Make letter appear in letter box
+            $(`#${letter} .word-letter`).text(clickedLetter);
+            $(`#${letter} .word-letter`).css('opacity','1');
+            check.push(1);
+          
+        } else if((i === currentWord.length - 1) && !check.length) {
+            // Decrement guesses if letter hasn't been guessed
+            if(!$(e.currentTarget).hasClass('letter-wrong')) {
+                guesses+=-1;
+                $('#guess-num').text(guesses);
+
+                // Change background of incorrectly guessed letter
+                $(e.currentTarget).addClass('letter-wrong');
+            }
+        } 
+    })
+    // Check if game won or lost
+    gameFinished();
+}

6906a652f2fc26ec9f31408929dde0a1c39ea8f0
Calculon9 <calcs1212@gmail.com>
2022-09-18 17:25:26 +0800

    Relocated a comment

--- main.js
+++ main.js
@@ -2,10 +2,8 @@ let words;
 let randomIndex;
 let currentWord;
 
-$(document).ready(function(){
-
-    // Fetch word list
-    
+ // Fetch word list upon page load
+$(document).ready(function(){   
     let req = new XMLHttpRequest();
 
     req.open("GET","https://raw.githubusercontent.com/dwyl/english-words/master/words_dictionary.json")
